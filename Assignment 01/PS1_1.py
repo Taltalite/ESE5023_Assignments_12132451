8c9d84c23c4f5462db106a2bc7c888ba096bedb7
Taltalite <leeeeegong@hotmail.com>
2021-10-04 17:06:46 +0800

    add problem 1 solution

---
+++ Assignment 01/PS1_1.py
@@ -0,0 +1,22 @@
+import random
+
+
+def Print_values(a, b, c):
+    if a > b:
+        if b > c:
+            print('a: %s, b: %s, c: %s.' % (a, b, c))
+        elif a > c:
+            print('a: %s, c: %s, b: %s.' % (a, c, b))
+        else:
+            print('c: %s, a: %s, b: %s.' % (c, a, b))
+    elif not b > c:
+        print('c: %s, b: %s, a: %s.' % (c, b, a))
+    return
+
+
+if __name__ == "__main__":
+    a = random.randint(0, 10000)
+    b = random.randint(0, 10000)
+    c = random.randint(0, 10000)
+    # print(a, b, c)
+    Print_values(a, b, c)
